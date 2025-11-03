14d53019216510e6ca1f254a4278917b6c6af921
Anirban Bandyopadhyay <bandyopadhyay4u@gmail.com>
2025-11-04 00:15:21 +0530

    Day 5: PDF uploads, search, navbar, footer, UI polish

--- core/views.py
+++ core/views.py
@@ -1,16 +1,17 @@
-from django.shortcuts import render
-from .models import Subject
-
-def home(request):
-    est_subjects = Subject.objects.filter(exam_type='EST')
-    return render(request, 'home.html', {'subjects': est_subjects})
-
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from .models import Subject
 
 def home(request):
-    est_subjects = Subject.objects.filter(exam_type='EST')
-    return render(request, 'home.html', {'subjects': est_subjects})
+    query = request.GET.get('q')
+    if query:
+        subjects = Subject.objects.filter(
+            Q(name__icontains=query) | Q(description__icontains=query),
+            exam_type='EST'  # ← You’re using EST, not MST-1
+        )
+    else:
+        subjects = Subject.objects.filter(exam_type='EST')
+    return render(request, 'home.html', {'subjects': subjects, 'query': query})
 
 def subject_detail(request, subject_id):
     subject = get_object_or_404(Subject, id=subject_id)
